b902e80da35aef9c9a309072d950a6577df34b6f
angelo <angelowilliams1015@gmail.com>
2023-05-12 11:40:38 +0200

    update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -28,7 +28,7 @@ const Portfolio = () => {
                     <div className="portfolio__item-image">
                         <img src={img5} alt="" />
                     </div>
-                    <h3>Coffee store mobile responsive site created with HTML AND CSS</h3>
+                    <h3>Coffee store mobile responsive site created with HTML, CSS and JavaScript.</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/williamsgelo/coffee-store" className='btn' target='_blank' rel="noreferrer">Github</a>
                         <a href="https://williamsgelo.github.io/coffee-store" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
@@ -39,7 +39,7 @@ const Portfolio = () => {
                     <div className="portfolio__item-image">
                         <img src={img4} alt="" />
                     </div>
-                    <h3>Language translator application build with JavaScript</h3>
+                    <h3>Language translator application build with JavaScript.</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/williamsgelo/language-translator" className='btn' target='_blank' rel="noreferrer">Github</a>
                         <a href="https://williamsgelo.github.io/language-translator" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
@@ -61,7 +61,7 @@ const Portfolio = () => {
                     <div className="portfolio__item-image">
                         <img src={img7} alt="" />
                     </div>
-                    <h3>Mordern UI/UX business app build with ReactJs and Tailwind</h3>
+                    <h3>Mordern UI/UX business app build with ReactJs and Tailwind.</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/williamsgelo/mordern-bank" className='btn' target='_blank' rel="noreferrer">Github</a>
                         <a href="https://mordern-bank-ten.vercel.app" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
@@ -71,7 +71,7 @@ const Portfolio = () => {
                     <div className="portfolio__item-image">
                         <img src={img9} alt="" />
                     </div>
-                    <h3>Fine Dining mobile-responsive mordern UI Restaurant Landing Page</h3>
+                    <h3>Fine Dining mobile-responsive mordern UI Restaurant Landing Page.</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/williamsgelo/restaurant-ui" className='btn' target='_blank' rel="noreferrer">Github</a>
                         <a href="https://williamsgelo.github.io/restaurant-ui" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
@@ -91,7 +91,7 @@ const Portfolio = () => {
                     <div className="portfolio__item-image">
                         <img src={img12} alt="" />
                     </div>
-                    <h3>OPEN AI image generation app build with OPEN AI and MongoDB backend</h3>
+                    <h3>OPEN AI image generation app build with OPEN AI and MongoDB backend.</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/williamsgelo/dall-e" className='btn' target='_blank' rel="noreferrer">Github</a>
                         <a href="https://dall-e-gold.vercel.app" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
@@ -101,7 +101,7 @@ const Portfolio = () => {
                     <div className="portfolio__item-image">
                         <img src={img11} alt="" />
                     </div>
-                    <h3>Mordern movie UI search build app with MovieDB API</h3>
+                    <h3>Mordern movie UI search build app with MovieDB API.</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/williamsgelo/movieapp" className='btn' target='_blank' rel="noreferrer">Github</a>
                         <a href="https://williamsgelo.github.io/movieapp" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
@@ -121,7 +121,7 @@ const Portfolio = () => {
                     <div className="portfolio__item-image">
                         <img src={img13} alt="" />
                     </div>
-                    <h3>Mobile repsonsive multi-choice quiz app build with ReactJs</h3>
+                    <h3>Mobile repsonsive multi-choice quiz app build with ReactJs.</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/williamsgelo/quizapp" className='btn' target='_blank' rel="noreferrer">Github</a>
                         <a href="https://williamsgelo.github.io/quizapp" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
@@ -131,7 +131,7 @@ const Portfolio = () => {
                     <div className="portfolio__item-image">
                         <img src={img14} alt="" />
                     </div>
-                    <h3>Mobile responsive fun memory game build with ReactJs</h3>
+                    <h3>Mobile responsive fun memory game build with ReactJs.</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/williamsgelo/magic-memory" className='btn' target='_blank' rel="noreferrer">Github</a>
                         <a href="https://williamsgelo.github.io/magic-memory" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
@@ -141,7 +141,7 @@ const Portfolio = () => {
                     <div className="portfolio__item-image">
                         <img src={img15} alt="" />
                     </div>
-                    <h3>Mobile-responsive stripe menu build with ReactJs</h3>
+                    <h3>Mobile-responsive stripe menu build with ReactJs.</h3>
                     <div className="portfolio__item-cta">
                         <a href="https://github.com/williamsgelo/stripe-menu" className='btn' target='_blank' rel="noreferrer">Github</a>
                         <a href="https://williamsgelo.github.io/stripe-menu" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
